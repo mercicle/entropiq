22f3c5d7a42868ce10a791a40f048859e8eb93cf
John Mercer <presci3nce@gmail.com>
2021-07-19 06:08:10 -0400

    qml updates

---
+++ quantum-machine-learning/helpers/helper_functions.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: mercicle
+"""
+
+import pandas as pd
+import numpy as np
+
+def bond_parser(in_mol2_lines):
+
+    f_text = ''.join(in_mol2_lines)
+    f_text = f_text.split('@<TRIPOS>BOND')[1]
+
+    # check for more @<TRIPOS> and if exists then take the first element
+
+    has_more_sections = len(f_text.split('@<TRIPOS>')) > 1
+
+    if has_more_sections:
+        print("Found more sections after @<TRIPOS>BOND")
+        f_text = f_text.split('@<TRIPOS>')[0]
+
+    row_chunks = f_text.split('\n')[1:]
+    df = pd.DataFrame()
+    for i in range(len(row_chunks)):
+        # i = 1
+        #print('i = '+str(i)+ '  row:' +row_chunks[i] )
+        edge_list = [x for x in row_chunks[i].split(" ") if x!='']
+        if len(edge_list)>0:
+            edge_list = edge_list[1:3]
+            df = df.append(pd.DataFrame.from_dict({'source':[edge_list[0]], 'target': [edge_list[1]]}))
+
+    return df
+
+def jaccard_sim(l1, l2):
+    intersection = len(list(set(l1).intersection(l2)))
+    union = len(l1) + len(l2) - intersection
+    return float(intersection) / union
---
+++ quantum-machine-learning/step0-setup.py
@@ -0,0 +1,33 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@author: mercicle
+"""
+
+##########################################
+###### Install Python Libraries ##########
+##########################################
+
+# !pip install dwave-ocean-sdk networkx
+# !pip install numpy --upgrade
+# !pip install biopandas
+
+# This is important pymol read_mol2_from_list will error without it.
+# !pip install pandas==0.24
+# !pip install git+git://github.com/rasbt/biopandas.git
+# !pip install pandas-compat
+
+# !pip install sh glob
+
+##########################################
+########### DWave Ocean API  #############
+##########################################
+
+# Step 1: From https://docs.ocean.dwavesys.com/en/stable/overview/sapi.html#sapi-access, you have to run from terminal
+# $ dwave config create
+# and provide your API token and other info
+    
+    
+    
