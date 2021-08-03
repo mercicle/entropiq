b0fcd94021c4fc8f2fca96c576aeec9bc86c9719
John Mercer <presci3nce@gmail.com>
2021-08-03 12:19:43 -0400

    cleaning up some dwave files from past project and reusing some for new mis comparison
    cleaning up some dwave files from past project and reusing some for new mis comparison

---
+++ quantum-machine-learning/dwave-experiments/dwave-mis-illustration/README.md
@@ -0,0 +1,124 @@
+# Quantum Computing Final Project
+
+Welcome to my QC Project 1 code base!
+
+# Learning DWave
+
+Resources available in Leap: https://cloud.dwavesys.com/leap/resources
+Ocean Documentation:  https://docs.ocean.dwavesys.com/en/stable/getting_started.html#gs
+Ocean Install Videos:  https://www.youtube.com/playlist?list=PLPvKnT7dgEsuhec_yW9oxcZ6gg9SfRFFV
+Ocean Code Examples:  https://github.com/dwave-examples
+Introduction to D-Wave and Quantum Annealing: https://docs.dwavesys.com/docs/latest/doc_getting_started.html
+Problem Solving Handbook: https://docs.dwavesys.com/docs/latest/doc_handbook.html
+
+
+# Repository Structure
+
+The helpers functions in /helpers has the bond_parser and jaccard_sim functions created for my analysis.
+
+```
+./helpers
+./in-data
+./out-data
+```
+
+The scratchwork has my testing scipts for writing the bond_parser and also working with and scraping/transforming the .mol2 files.
+
+```
+./scratchwork
+./screenshots
+```
+
+This pipeline is the main pipeline. Note, for Step2 forward you must also signup for and retrieve your DWave API token and enter it via the command line with:
+
+```
+$ pip install dwave-ocean-sdk
+$ dwave config create
+
+```
+This .config file is located at:
+
+```
+nano /Users/mercicle/Library/Application\ Support/dwave/dwave.conf
+```
+
+This is the main pipeline.
+
+```
+step0-setup.py
+step1-raw-data-ingestion.py
+step2-dwave-and-classical-lib-experiments.py
+step3-compute-comparison-full-data.py
+step4-full-dwave-and-classical-testing.py
+step5-full-recalc-classical.py
+```
+
+
+Setup: Installation of necessary Python libraries.
+
+Raw Data Ingestion: programmatically unzipping all .tar.gz files and then all .mol2 files within the target sub-directories.
+
+DWave and Classical Graph Library Experiments: experimentation with both DWave and graph libraries, computing MIS, and visualizing the graphs with MIS encodings.
+
+Create the Full Atom and Bond Data Sets: Transforming the .mol2 text files into atom and atomic bond data frames.
+
+Conduct Full DWave vs. Classical Testing on Full Data: Iterating through the atomic bonds data frame and computing MIS with both DWave and classical library, consolidating the performance results, visualizing the MIS nodes on network visualizations, and conducting the distributional analysis of the Jaccard Similarity and run times.  
+
+Step 5 can be disregarded, I was going to attempt to recalculate based on different MIS library but will just make the previous codes more flexible in the future.
+
+# Data
+
+The in-data folder is git ignored so to download and replicate you must run the following while in in-data directory.
+
+```
+wget --base=http://blaster.docking.org/dud/r2/ -i - <<+
+ace.tar.gz
+ache.tar.gz
+ada.tar.gz
+alr2.tar.gz
+ampc.tar.gz
+ar.tar.gz
+cdk2.tar.gz
+comt.tar.gz
+cox1.tar.gz
+cox2.tar.gz
+dhfr.tar.gz
+egfr.tar.gz
+er_agonist.tar.gz
+er_antagonist.tar.gz
+fgfr1.tar.gz
+fxa.tar.gz
+gart.tar.gz
+gpb.tar.gz
+gr.tar.gz
+hivpr.tar.gz
+hivrt.tar.gz
+hmga.tar.gz
+hsp90.tar.gz
+inha.tar.gz
+mr.tar.gz
+na.tar.gz
+p38.tar.gz
+parp.tar.gz
+pde5.tar.gz
+pdgfrb.tar.gz
+pnp.tar.gz
+ppar.tar.gz
+pr.tar.gz
+rxr.tar.gz
+sahh.tar.gz
+src.tar.gz
+thrombin.tar.gz
+tk.tar.gz
+trypsin.tar.gz
+vegfr2.tar.gz
+energies.tar.gz
++
+```
+
+Then, you have to run these two scripts to unzip the tar files AND unzip the .mol .tar files from the unzipped tar files.
+
+```
+step0-setup.py
+step1-raw-data-ingestion.py
+```
---
+++ quantum-machine-learning/dwave-experiments/dwave-mis-illustration/step1_dwave_classical_mis_comparison.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: mercicle
+"""
+
+##########################################
+########## Standard Libraries  ###########
+##########################################
+
+import time
+import networkx as nx
+import matplotlib
+matplotlib.use("agg")
+import matplotlib.pyplot as plt
+
+##########################################
+##########    DWave Libraries  ###########
+##########################################
+
+# DWave libs
+# DWaveSampler(solver={'qpu': True})
+# ^^^ this is already True based on my selections in the command line setup.
+
+import dwave_networkx as dnx
+from dwave.system.samplers import DWaveSampler
+from dwave.system.composites import EmbeddingComposite
+
+########################################
+##            Setup Parms             ##
+########################################
+
+original_name = "original_network.png"
+solution_name = "solution_annotated_network.png"
+
+save_results = False
+sampler = EmbeddingComposite(DWaveSampler())
+
+n_sim_nodes = 20
+
+########################################
+##           Simulate a Graph         ##
+########################################
+
+# first simulate a scale-free grapha nd them coerce to undirected
+G = nx.scale_free_graph(n_sim_nodes)
+G = nx.DiGraph.to_undirected(G)
+print(nx.info(G))
+newtork_layout = nx.spring_layout(G)
+
+##################################################
+## Cmpute MIS with Both DWave and Classical     ##
+##################################################
+
+# Compute MIS
+start_time = time.time()
+dwave_mis_results = dnx.maximum_independent_set(G, sampler = sampler, num_reads = 10)
+end_time = time.time()
+total_seconds = (end_time - start_time)
+print("QA Compute time in {} seconds.".format(total_seconds))
+
+
+size_dwave_mis = len(dwave_mis_results)
+
+start_time = time.time()
+classical_mis_results = nx.maximal_independent_set(G)
+end_time = time.time()
+total_seconds = (end_time - start_time)
+print("Classical ompute time in {} seconds.".format(total_seconds))
+
+size_classical_mis = len(classical_mis_results)
+
+# Assess Results
+print('MIS: DWave n=', size_dwave_mis, ' and Classical n= ', size_classical_mis)
+print(dwave_mis_results)
+
+########################################
+##     Visualize DWave Result         ##
+########################################
+# Compute subgraphs for network viz annotations
+dwave_mis_subgraph = G.subgraph(list(dwave_mis_results)).copy()
+print(nx.info(dwave_mis_subgraph))
+
+nodes_not_in_dwave_mis = list(set(G.nodes()) - set(dwave_mis_results))
+not_dwave_mis_subgraph = G.subgraph(nodes_not_in_dwave_mis)
+print(nx.info(not_dwave_mis_subgraph))
+
+plt.figure()
+
+# Save
+
+nx.draw_networkx(G,
+                 pos = newtork_layout,
+                 with_labels = True)
+if save_results:
+    plt.savefig(original_name, bbox_inches='tight')
+
+nx.draw_networkx(dwave_mis_subgraph,
+                 pos=newtork_layout,
+                 with_labels=True,
+                 node_color='r',
+                 font_color='k')
+nx.draw_networkx(not_dwave_mis_subgraph,
+                 pos=newtork_layout,
+                 with_labels=True,
+                 node_color='grey',
+                 font_color='w')
+
+if save_results:
+    plt.savefig('dwave_'+solution_name, bbox_inches='tight')
+
+
+########################################
+##     Visualize Classical Result     ##
+########################################
+
+# Compute subgraphs for network viz annotations
+classical_mis_subgraph = G.subgraph(list(classical_mis_results)).copy()
+print(nx.info(classical_mis_subgraph))
+
+nodes_not_in_classical_mis = list(set(G.nodes()) - set(classical_mis_results))
+not_classical_mis_subgraph = G.subgraph(nodes_not_in_classical_mis)
+print(nx.info(not_classical_mis_subgraph))
+
+
+plt.figure()
+
+# Save
+plt.title('foo')
+nx.draw_networkx(G,
+                 pos = newtork_layout,
+                 with_labels = True)
+
+nx.draw_networkx(classical_mis_subgraph,
+                 pos=newtork_layout,
+                 with_labels=True,
+                 node_color='r',
+                 font_color='k')
+nx.draw_networkx(not_classical_mis_subgraph,
+                 pos=newtork_layout,
+                 with_labels=True,
+                 node_color='grey',
+                 font_color='w')
+
+if save_results:
+    plt.savefig('classical_'+solution_name, bbox_inches='tight')
