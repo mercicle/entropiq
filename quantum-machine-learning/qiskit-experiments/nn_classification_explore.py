1f5d8afd6ed7901cf162822096b5233c1a06b53e
John Mercer <presci3nce@gmail.com>
2021-08-08 13:18:31 -0400

    rewriting the classification script
    rewriting the classification script

--- quantum-machine-learning/qiskit-experiments/nn_classification_explore.py
+++ quantum-machine-learning/qiskit-experiments/nn_classification_explore.py
@@ -20,16 +20,16 @@ from qiskit_machine_learning.exceptions import QiskitMachineLearningError
 from typing import Union
 
 
-quantum_instance = QuantumInstance(Aer.get_backend('qasm_simulator'), shots=1024)
+quantum_instance = QuantumInstance(Aer.get_backend('qasm_simulator'), shots = 1024)
+
+# parity maps bitstrings to 0 or 1
+def parity(x):
+    return '{:b}'.format(x).count('1') % 2
 
 
-# ## Classification
-# 
-# We prepare a simple classification dataset to illustrate the following algorithms.
-
-# In[3]:
-
-
+########################################
+########################################
+########################################
 num_inputs = 2
 num_samples = 20
 X = 2*np.random.rand(num_samples, num_inputs) - 1
@@ -47,44 +47,24 @@ for x, y_target in zip(X, y):
 plt.plot([-1, 1], [1, -1], '--', color='black')
 plt.show()
 
-
-# ### Classification with the an `OpflowQNN`
-# 
-# First we show how an `OpflowQNN` can be used for classification within a `NeuralNetworkClassifier`. In this context, the `OpflowQNN` is expected to return one-dimensional output in $[-1, +1]$. This only works for binary classification and we assign the two classes to $\{-1, +1\}$. For convenience, we use the `TwoLayerQNN`, which is a special type of `OpflowQNN` defined via a feature map and an ansatz.
-
-# In[4]:
-
+########################################
+##########   OpflowQNN   ###############
+########################################
 
 # construct QNN
 opflow_qnn = TwoLayerQNN(num_inputs, quantum_instance=quantum_instance)
 
-
-# In[5]:
-
-
 # QNN maps inputs to [-1, +1]
 opflow_qnn.forward(X[0, :], np.random.rand(opflow_qnn.num_weights))
 
-
-# In[6]:
-
-
 # construct neural network classifier
 opflow_classifier = NeuralNetworkClassifier(opflow_qnn, optimizer=COBYLA())
-
-
-# In[7]:
-
 
 # fit classifier to data
 opflow_classifier.fit(X, y)
 
 # score classifier
 opflow_classifier.score(X, y)
-
-
-# In[8]:
-
 
 # evaluate data points
 y_predict = opflow_classifier.predict(X)
@@ -101,13 +81,13 @@ for x, y_target, y_p in zip(X, y, y_predict):
 plt.plot([-1, 1], [1, -1], '--', color='black')
 plt.show()
 
-
-# ### Classification with a `CircuitQNN`
-# 
-# Next we show how a `CircuitQNN` can be used for classification within a `NeuralNetworkClassifier`. In this context, the `CircuitQNN` is expected to return $d$-dimensional probability vector as output, where $d$ denotes the number of classes. Sampling from a `QuantumCircuit` automatically results in a probability distribution and we just need to define a mapping from the measured bitstrings to the different classes. For binary classification we use the parity mapping.
-
-# In[9]:
-
+########################################
+##########   CircuitQNN   ###############
+########################################
+# In this context, the `CircuitQNN` is expected to return $d$-dimensional probability vector as output, 
+# where $d$ denotes the number of classes. Sampling from a `QuantumCircuit` automatically results in a 
+# probability distribution and we just need to define a mapping from the measured bitstrings to the different classes. 
+# For binary classification we use the parity mapping.
 
 # construct feature map
 feature_map = ZZFeatureMap(num_inputs)
@@ -121,18 +101,7 @@ qc.append(feature_map, range(num_inputs))
 qc.append(ansatz, range(num_inputs))
 qc.decompose().draw(output='mpl')
 
-
-# In[10]:
-
-
-# parity maps bitstrings to 0 or 1
-def parity(x):
-    return '{:b}'.format(x).count('1') % 2
 output_shape = 2  # corresponds to the number of classes, possible outcomes of the (parity) mapping.
-
-
-# In[11]:
-
 
 # construct QNN
 circuit_qnn = CircuitQNN(circuit=qc, 
@@ -142,27 +111,15 @@ circuit_qnn = CircuitQNN(circuit=qc,
                          output_shape=output_shape,
                          quantum_instance=quantum_instance)
 
-
-# In[12]:
-
-
 # construct classifier
 circuit_classifier = NeuralNetworkClassifier(neural_network=circuit_qnn,                                             
                                              optimizer=COBYLA())
-
-
-# In[13]:
-
 
 # fit classifier to data
 circuit_classifier.fit(X, y01)
 
 # score classifier
 circuit_classifier.score(X, y01)
-
-
-# In[14]:
-
 
 # evaluate data points
 y_predict = circuit_classifier.predict(X)
@@ -178,59 +135,4 @@ for x, y_target, y_p in zip(X, y01, y_predict):
         plt.scatter(x[0], x[1], s=200, facecolors='none', edgecolors='r', linewidths=2)
 plt.plot([-1, 1], [1, -1], '--', color='black')
 plt.show()
-
-
-# ### Variational Quantum Classifier (`VQC`)
-# 
-# The `VQC` is a special variant of the `NeuralNetworkClassifier` with a `CircuitQNN`. It applies a parity mapping (or extensions to multiple classes) to map from the bitstring to the classification, which results in a probability vector, which is interpreted as a one-hot encoded result. By default, it applies this the `CrossEntropyLoss` function that expects labels given in one-hot encoded format and will return predictions in that format too.
-
-# In[15]:
-
-
-# construct feature map, ansatz, and optimizer
-feature_map = ZZFeatureMap(num_inputs)
-ansatz = RealAmplitudes(num_inputs, reps=1)
-
-# construct variational quantum classifier
-vqc = VQC(feature_map=feature_map,
-          ansatz=ansatz, 
-          loss='cross_entropy',
-          optimizer=COBYLA(), 
-          quantum_instance=quantum_instance)
-
-
-# In[16]:
-
-
-# fit classifier to data
-vqc.fit(X, y_one_hot)
-
-# score classifier
-vqc.score(X, y_one_hot)
-
-
-# In[17]:
-
-
-# evaluate data points
-y_predict = vqc.predict(X)
-
-# plot results
-# red == wrongly classified
-for x, y_target, y_p in zip(X, y_one_hot, y_predict):
-    if y_target[0] == 1:
-        plt.plot(x[0], x[1], 'bo')
-    else:
-        plt.plot(x[0], x[1], 'go')
-    if not np.all(y_target == y_p):
-        plt.scatter(x[0], x[1], s=200, facecolors='none', edgecolors='r', linewidths=2)
-plt.plot([-1, 1], [1, -1], '--', color='black')
-plt.show()
-
-
-
-# In[ ]:
-
-
-
 
