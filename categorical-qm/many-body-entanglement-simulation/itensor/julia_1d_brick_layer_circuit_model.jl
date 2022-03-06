e914fc5993d655e8da381c96ecbf76abcb379d2d
John Mercer <presci3nce@gmail.com>
2022-03-05 21:38:46 -0500

    julia implementation - cleaning up and unfactoring another functions

--- categorical-qm/many-body-entanglement-simulation/itensor/julia_1d_brick_layer_circuit_model.jl
+++ categorical-qm/many-body-entanglement-simulation/itensor/julia_1d_brick_layer_circuit_model.jl
@@ -11,17 +11,31 @@
 # ρ = prime(ϕ, tags = "Site")
 # https://www.itensor.org/docs.cgi?vers=cppv2&page=tutorials/primes
 
+# DataFrames
+# https://github.com/bkamins/Julia-DataFrames-Tutorial/
+# https://www.ahsmart.com/pub/data-wrangling-with-data-frames-jl-cheat-sheet/
+#DataFrame(A=1:3, B=rand(3), C=randstring.([3,3,3]), fixed=1)
+
+[x; x]
 Pkg.add("StatsBase")
 Pkg.add("Distributions")
+Pkg.add("DataFrames")
+Pkg.add("CSV")
+
 
 using PastaQ
 using ITensors
 using Random
 using Printf
 using LinearAlgebra
-using StatsBase: mean, sem, sample as stats_sample, Weights
+using StatsBase: mean, sem
 
 using Distributions
+
+#
+using DataFrames
+using CSV
+
 
 # define the two measurement projectors
 import PastaQ: gate
@@ -33,27 +47,51 @@ gate(::GateName"Π1") =
    0 1]
 
 gate(::GateName"Π00") =
-[0, 0, 0, 0
- 0, 0, 0, 0
- 0, 0, 0, 0
- 0, 0, 0, 1]
+[0 0 0 0
+ 0 0 0 0
+ 0 0 0 0
+ 0 0 0 1]
 gate(::GateName"Π10") =
-[0, 0, 0, 0
- 0, 1, 0, 0
- 0, 0, 0, 0
- 0, 0, 0, 0]
+[0 0 0 0
+ 0 1 0 0
+ 0 0 0 0
+ 0 0 0 0]
 
 gate(::GateName"Π01") =
-[0, 0, 0, 0
- 0, 0, 0, 0
- 0, 0, 1, 0
- 0, 0, 0, 0]
+[0 0 0 0
+ 0 0 0 0
+ 0 0 1 0
+ 0 0 0 0]
 
 gate(::GateName"Π11") =
-[1, 0, 0, 0
- 0, 0, 0, 0
- 0, 0, 0, 0
- 0, 0, 0, 0]
+[1 0 0 0
+ 0 0 0 0
+ 0 0 0 0
+ 0 0 0 0]
+
+ # Von Neumann entropy at center bond
+ # https://en.wikipedia.org/wiki/Von_Neumann_entropy
+ # http://www.scholarpedia.org/article/Quantum_entropies
+ # "von Neumann entropy is a limiting case of the Rényi entropy" lim α→1 Sα(ρ) = S(ρ)
+ # Given a family of entropies {Sα(ρ)}α, where α is some index, the entropies are monotonic in α∈ℝ
+
+ function entanglemententropy(ψ₀::MPS)
+
+   ψ = normalize!(copy(ψ₀))
+   N = length(ψ)
+   bond = N ÷ 2
+   orthogonalize!(ψ, bond)
+
+   row_inds = (linkind(ψ, bond - 1), siteind(ψ, bond))
+   u, s, v = svd(ψ[bond], row_inds)
+
+   S = 0.0
+   for n in 1:dim(s, 1)
+     λ = s[n, n]^2
+     S -= λ * log(λ + 1e-20)
+   end
+   return S
+ end
 
 let
 
@@ -64,36 +102,43 @@ let
   measurement_rate_space = 0.0:0.02:0.2
   simulation_space = 1:n_simulations
   layer_space = 1:n_layers
-  do_single_qubit_projections = true
-  projective_list = [ "00"; "01"; "10"; "11"]
 
+  do_single_qubit_projections = true
   qubit_index_space = nothing
   if do_single_qubit_projections
-    qubit_index_space = 1:num_monitored_qubits
+    qubit_index_space = 1:num_qubits
   else
-    qubit_index_space = 1:(num_monitored_qubits-1)
+    qubit_index_space = 1:(num_qubits-1)
   end
 
-  circuits_results = []
+  projective_list = [ "00"; "01"; "10"; "11"]
+
+  simulation_df = DataFrame()
+
+  circuit_simulations = []
   for this_sim in simulation_space
-    this_layer_results = []
+    layers = []
     for this_layer in layer_space
       @printf("Simulation = %.3i  Layer = %.3i \n", this_sim, this_layer)
-      this_entangled_layer = entangling_layer(num_qubits)
-      push!(this_layer_results, this_entangled_layer)
+
+      layer_odd  = randomlayer("RandomUnitary",[(j,j+1) for j in 1:2:num_qubits-1])
+      layer_even = randomlayer("RandomUnitary",[(j,j+1) for j in 2:2:num_qubits-1])
+      this_entangled_layer = [layer_odd..., layer_even...]
+
+      push!(layers, this_entangled_layer)
     end
-    push!(circuits_results, this_layer_results)
+    push!(circuit_simulations, layers)
   end
 
   # loop over projective measurement probability (per site)
   for measurement_rate in measurement_rate_space
 
       svn = []
-      num_monitored_qubits = nqubits(circuits_results[1])
-      for this_circuit in circuits_results
+      num_qubits = nqubits(circuit_simulations[1])
+      for this_circuit in circuit_simulations
 
          # initialize state ψ = |000…⟩
-         ψ = productstate(num_monitored_qubits)
+         ψ = productstate(num_qubits)
 
          # sweep over layers
          for this_layer in this_circuit
@@ -120,7 +165,7 @@ let
                    ψ = orthogonalize!(ψ, qubit_index:next_qubit_index)
                    ϕ = ψ[qubit_index:next_qubit_index]
 
-                   ρ = prime(ϕ, tags = "Site") * dag(ϕ) # 1-qubit reduced density matrix
+                   ρ = prime(ϕ, tags = "Site") * dag(ϕ)
                    unitary_pmf = real.(diag(array(ρ))) # Outcome probabilities
                    σ = wsample(projective_list, unitary_pmf, 1)[1]
                end
@@ -129,84 +174,21 @@ let
                ψ = runcircuit(ψ, (projection_string, qubit_index)) # Projection
                normalize!(ψ)
              end # if measurement_rate > rand()
-           end # for monitored_qubit_index in 1:num_monitored_qubits
+           end # for monitored_qubit_index in 1:num_qubits
          end # for this_layer in this_circuit
          push!(svn, entanglemententropy(ψ))
 
-      end # for this_circuit in circuits_results
-      @printf("Measurement Rate = %.2f  S(ρ) = %.5f ± %.1E \n", measurement_rate, mean(svn), sem(svn))
+      end # for this_circuit in circuit_simulations
+
+      mean_entropy = mean(svn)
+      se_mean_entropy = sem(svn)
+      @printf("Measurement Rate = %.2f  S(ρ) = %.5f ± %.1E \n", measurement_rate, mean_entropy, se_mean_entropy)
+
+      #append(pd.DataFrame.from_dict({'simulation': [this_simulation], 'num_qubits': [num_qubits], 'measurement_rate':[measurement_rate],
+      #'layer': [this_layer],'keep_layer': [keep_layer], 'renyi_entropy_2nd': [renyi_entropy_2nd] }))
+      this_simulation_df = DataFrame(measurement_rate = measurement_rate, mean_entropy = mean_entropy, se_mean_entropy=se_mean_entropy)
+      simulation_df = [simulation_df; this_simulation_df]
 
     end # for measurement_rate in measurement_rate_space
 
 end # let scope
-
-# Von Neumann entropy at center bond
-# https://en.wikipedia.org/wiki/Von_Neumann_entropy
-# http://www.scholarpedia.org/article/Quantum_entropies
-# "von Neumann entropy is a limiting case of the Rényi entropy" lim α→1 Sα(ρ) = S(ρ)
-# Given a family of entropies {Sα(ρ)}α, where α is some index, the entropies are monotonic in α∈ℝ
-
-function entanglemententropy(ψ₀::MPS)
-
-  ψ = normalize!(copy(ψ₀))
-  N = length(ψ)
-  bond = N ÷ 2
-  orthogonalize!(ψ, bond)
-
-  row_inds = (linkind(ψ, bond - 1), siteind(ψ, bond))
-  u, s, v = svd(ψ[bond], row_inds)
-
-  S = 0.0
-  for n in 1:dim(s, 1)
-    λ = s[n, n]^2
-    S -= λ * log(λ + 1e-20)
-  end
-  return S
-end
-
-# build a brick-layer of random unitaries covering all
-# nearest-neighbors bonds
-function entangling_layer(N::Int)
-  layer_odd  = randomlayer("RandomUnitary",[(j,j+1) for j in 1:2:N-1])
-  layer_even = randomlayer("RandomUnitary",[(j,j+1) for j in 2:2:N-1])
-  return [layer_odd..., layer_even...]
-end
-
-# perform a projective measurement at a given site
-function projective_measurement!(ψ₀::MPS, site::Int)
-  ψ = orthogonalize!(ψ₀, site)
-  ϕ = ψ[site]
-  # 1-qubit reduced density matrix
-  ρ = prime(ϕ, tags="Site") * dag(ϕ)
-  # Outcome probabilities
-  prob = real.(diag(array(ρ)))
-  # Sample
-  σ = (rand() < prob[1] ? 0 : 1)
-  # Projection
-  ψ = runcircuit(ψ, ("Π"*"$(σ)", site))
-  normalize!(ψ)
-  ψ₀[:] = ψ
-  return ψ₀
-end
-
-# compute average Von Neumann entropy for an ensemble of random circuits
-# at a given local measurement probability rate
-function monitored_circuits(circuits::Vector{<:Vector}, p::Float64)
-  svn = []
-  num_monitored_qubits = nqubits(circuits[1])
-  for circuit in circuits
-    # initialize state ψ = |000…⟩
-    ψ = productstate(num_monitored_qubits)
-    # sweep over layers
-    for layer in circuit
-      # apply entangling unitary
-      ψ = runcircuit(ψ, layer; cutoff = 1e-8)
-      # perform measurements
-      for j in 1:num_monitored_qubits
-        p > rand() && projective_measurement!(ψ, j)
-      end
-    end
-    push!(svn, entanglemententropy(ψ))
-  end
-  return svn
-end
