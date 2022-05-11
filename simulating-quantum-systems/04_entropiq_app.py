4d1d31ddde7b7883e20f00ff979abbd3c0dc879c
John Mercer <presci3nce@gmail.com>
2022-05-11 12:08:15 -0400

    paper updates
    paper updates

--- simulating-quantum-systems/04_entropiq_app.py
+++ simulating-quantum-systems/04_entropiq_app.py
@@ -201,7 +201,7 @@ elif selected == "Discovery":
                              "mean_entropy": "Average Entanglement Entropy"
                          })
     main_fig.update_traces(line = dict(width=3))
-    main_fig.update_layout(font = dict(size=20))
+    main_fig.update_layout(font = dict(size=20),legend_title_text='# of Qubits')
 
     main_fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
     st.plotly_chart(main_fig, use_container_width=True)
@@ -218,7 +218,8 @@ elif selected == "Discovery":
                              "mean_runtime_min": "Average Simulation Runtime (Min)"
                          })
     main_fig.update_traces(line = dict(width=3))
-    main_fig.update_layout(font = dict(size=20))
+    main_fig.update_layout(font = dict(size=20),
+                           legend_title_text='# of Qubits')
     main_fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
     st.plotly_chart(main_fig, use_container_width=True)
 
@@ -227,6 +228,7 @@ elif selected == "Discovery":
     print("Dropping Experiment ID")
     entropy_tracking_df.drop(columns=['experiment_id'],inplace=True)
     entropy_tracking_df['log_state_index'] = entropy_tracking_df['ij'].apply(lambda x: np.log(x))
+    entropy_tracking_df['num_qubits'] = entropy_tracking_df['num_qubits'].astype(int)
 
     st.subheader('Entropy Contribution by Measurement Rate and System Size')
     st.subheader('Preview')
@@ -235,30 +237,66 @@ elif selected == "Discovery":
 
     n_sim_color_palette = list(Color("#c7e9c0").range_to(Color("#006d2c"),n_simulations))
     n_sim_color_palette = np.flip([c.hex for c in n_sim_color_palette])
-    et_fig = px.line(entropy_tracking_df,
-                     x='log_state_index',
-                     y='entropy_contribution',
-                     color='simulation_number',
-                     color_discrete_sequence=n_sim_color_palette,
-                     facet_col='measurement_rate',
-                     facet_row='num_qubits',
-                     height=800, width=800,
-                     labels={
-                         "entropy_contribution": "ΔS",
-                         "log_state_index": "Log(State Index)"
-                      },
-                     category_orders={
-                     "simulation_number": np.sort(entropy_tracking_df.simulation_number.unique()).tolist(),
-                     "num_qubits": np.sort(entropy_tracking_df.num_qubits.unique()).tolist(),
-                     "measurement_rate": np.sort(entropy_tracking_df.measurement_rate.unique()).tolist()
-                     })
+    # et_fig = px.line(entropy_tracking_df,
+    #                  x='log_state_index',
+    #                  y='entropy_contribution',
+    #                  color='simulation_number',
+    #                  color_discrete_sequence=n_sim_color_palette,
+    #                  facet_col='measurement_rate',
+    #                  facet_row='num_qubits',
+    #                  height=800, width=800,
+    #                  labels={
+    #                      "entropy_contribution": "ΔS",
+    #                      "log_state_index": "Log(State Index)"
+    #                   },
+    #                  category_orders={
+    #                  "simulation_number": np.sort(entropy_tracking_df.simulation_number.unique()).tolist(),
+    #                  "num_qubits": np.sort(entropy_tracking_df.num_qubits.unique()).tolist(),
+    #                  "measurement_rate": np.sort(entropy_tracking_df.measurement_rate.unique()).tolist()
+    #                  })
+    #
+    # et_fig.update_layout(legend=dict(orientation="h"))
+    # et_fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
+    # et_fig.update_yaxes(matches=None)
+    # et_fig.update_xaxes(matches=None)
+    #
+    # st.plotly_chart(et_fig, use_container_width=True)
 
-    et_fig.update_layout(legend=dict(orientation="h"))
-    et_fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
-    et_fig.update_yaxes(matches=None)
-    et_fig.update_xaxes(matches=None)
+    st.subheader('Histogram - Evolution of State Probabilities')
 
-    st.plotly_chart(et_fig, use_container_width=True)
+    eti_fig_hist = px.histogram(entropy_tracking_df,
+                                 #y="num_qubits",
+                                 x="eigenvalue",
+                                 facet_col="num_qubits",
+                                 facet_col_wrap = 4,
+                                 color="num_qubits",
+                                 color_discrete_sequence = n_qubit_color_palette,
+
+                                 #barnorm = 'fraction',
+                                 histnorm = 'probability',
+                                 nbins = 20,
+                                 # add the animation
+                                 animation_frame="measurement_rate",
+                                 category_orders={
+                                 "num_qubits": np.sort(entropy_tracking_df.num_qubits.unique()).tolist(),
+                                 },
+                                 labels={
+                                      "eigenvalue": "State Probability",
+                                  },
+                                 # anchor the ranges so that the chart doesn't change frame to frame
+                                 range_x=[0,entropy_tracking_df["eigenvalue"].max()*1.05],
+                                 range_y = [0,1],
+                                 height=800, width=800,
+    )
+
+    eti_fig_hist.for_each_yaxis(lambda y: y.update(title = ''))
+    eti_fig_hist.update_traces(marker_line_width=1,marker_line_color="white")
+    eti_fig_hist.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
+    eti_fig_hist.update_layout(font = dict(size=20),
+                               legend=dict(orientation="h",yanchor="bottom",y=1.02,xanchor="right",x=1),
+                               yaxis_title = "%",
+                               legend_title_text='# of Qubits')
+    st.plotly_chart(eti_fig_hist, use_container_width=True)
 
     st.subheader('Inspection')
 
@@ -267,20 +305,55 @@ elif selected == "Discovery":
 
     inspect_entropy_tracking_df = entropy_tracking_df[ (entropy_tracking_df.num_qubits == select_nq) & (entropy_tracking_df.measurement_rate == select_mr)]
 
-    eti_fig = px.line(inspect_entropy_tracking_df,
-                  x='log_state_index',
-                  y='entropy_contribution',
-                  color='simulation_number',
-                  color_discrete_sequence=n_sim_color_palette,
-                  facet_col='measurement_rate',
-                  facet_row='num_qubits',
-                  height=800, width=800,
-                  labels={
-                       "entropy_contribution": "ΔS",
-                       "log_state_index": "Log(State Index)"
-                   })
-    eti_fig.update_traces(line = dict(width=3))
-    eti_fig.update_layout(font = dict(size=20), legend = dict(orientation="h"))
+    # st.subheader('Inspection - Curves')
+    #
+    # eti_fig = px.line(inspect_entropy_tracking_df,
+    #               x='log_state_index',
+    #               y='entropy_contribution',
+    #               color='simulation_number',
+    #               color_discrete_sequence=n_sim_color_palette,
+    #               facet_col='measurement_rate',
+    #               facet_row='num_qubits',
+    #               height=800, width=800,
+    #               labels={
+    #                    "entropy_contribution": "ΔS",
+    #                    "log_state_index": "Log(State Index)"
+    #                })
+    # eti_fig.update_traces(line = dict(width=3))
+    # eti_fig.update_layout(font = dict(size=20), legend = dict(orientation="h"))
+    # eti_fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
+    # st.plotly_chart(eti_fig, use_container_width=True)
+
+    eti_fig = px.histogram(inspect_entropy_tracking_df,
+                             #y="num_qubits",
+                             x="eigenvalue",
+                             #facet_col="num_qubits",
+                             #facet_col_wrap = 4,
+                             #color="num_qubits",
+                             #color_discrete_sequence = n_qubit_color_palette,
+
+                             #barnorm = 'fraction',
+                             histnorm = 'probability',
+                             nbins = 20,
+                             # add the animation
+                             #animation_frame="measurement_rate",
+                             #category_orders={
+                             #"num_qubits": np.sort(entropy_tracking_df.num_qubits.unique()).tolist(),
+                             #},
+                             labels={
+                                  "eigenvalue": "State Probability",
+                              },
+                             # anchor the ranges so that the chart doesn't change frame to frame
+                             range_x=[0,entropy_tracking_df["eigenvalue"].max()*1.05],
+                             range_y = [0,1],
+                             height=800, width=800,
+    )
+
+    eti_fig.for_each_yaxis(lambda y: y.update(title = ''))
+    eti_fig.update_traces(marker_line_width=1,marker_line_color="white")
     eti_fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
+    eti_fig.update_layout(font = dict(size=20),
+                          legend=dict(orientation="h",yanchor="bottom",y=1.02,xanchor="right",x=1),
+                          yaxis_title = "%",
+                          legend_title_text='# of Qubits')
     st.plotly_chart(eti_fig, use_container_width=True)
-    print("End plotting entropy_contribution ΔS")
